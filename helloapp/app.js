81403852f50d103d79cdddb72395924f11886858
Alexander Golovko <alik@csltd.com.ua>
2018-02-25 12:35:49 +0200

    модули и экспортируемые из них объекты/функции

--- helloapp/app.js
+++ helloapp/app.js
@@ -1,8 +1,11 @@
-var http = require("http");
-http.createServer(function(request,response){
-     
-    response.end("Hello NodeJS!");
-     
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-}); 
+var os = require("os");
+var greeting = require("./greeting");
+
+var User = require("./user.js");
+var eugene = new User("Eugene", 32);
+eugene.sayHi();
+ 
+// получим имя текущего пользователя
+var userName = os.userInfo().username;
+console.log(`Дата запроса: ${greeting.date}`); // ё для кавычек строки, параметры ${} в которой должны интерполироваться
+console.log(greeting.getMessage(userName));
