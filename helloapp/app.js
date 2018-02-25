9367c91d1d59b67952bfa8ea3e66d70b25fb7ff8
Alexander Golovko <alik@csltd.com.ua>
2018-02-25 13:35:24 +0200

    package.json и устанавливаемые пакеты

--- helloapp/app.js
+++ helloapp/app.js
@@ -1,6 +1,12 @@
-var greeting = require("./greeting");
+// получаем модуль Express
+var express = require("express");
+// создаем приложение
+var app = express();
  
-global.name = "Eugene";
+// устанавливаем обработчик для маршрута "/"
+app.get("/", function(request, response){
  
-global.console.log(date);
-console.log(greeting.getMessage());
+    response.end("Hello from Express!");
+});
+// начинаем прослушивание подключений на 3000 порту
+app.listen(3000);
---
+++ helloapp/install_packages.bat
@@ -0,0 +1,10 @@
+rem устанавливает пакет express и добавляет ссылку на него в package.json.dependencies
+npm install express --save
+rem устанавливает пакет jasmine-node и добавляет ссылку на него в package.json.devDependencies
+npm install jasmine-node --save-dev
+rem устанавливает пакеты, ссылки на которые указаны в package.json
+rem npm install
+rem удаляет пакет express, но оставляет ссылку на него в package.json
+rem npm uninstall express
+rem удаляет пакет express и ссылку на него в package.json.dependencies
+rem npm uninstall express --save 
