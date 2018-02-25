b1500c3dfd51d3b056ec55813c2fae4f578c37ab
Alexander Golovko <alik@csltd.com.ua>
2018-02-25 13:39:05 +0200

    аргументы командной строки

--- helloapp/app.js
+++ helloapp/app.js
@@ -1,12 +1,10 @@
-// получаем модуль Express
-var express = require("express");
-// создаем приложение
-var app = express();
+var nodePath = process.argv[0];
+var appPath = process.argv[1];
+var name = process.argv[2];
+var age = process.argv[3];
  
-// устанавливаем обработчик для маршрута "/"
-app.get("/", function(request, response){
- 
-    response.end("Hello from Express!");
-});
-// начинаем прослушивание подключений на 3000 порту
-app.listen(3000);
+console.log("nodePath: " + nodePath);
+console.log("appPath: " + appPath);
+console.log();
+console.log("name: " + name);
+console.log("age: " + age);
