e36b474013c33f668d31f38626f9f20c37f96a67
Alexander Golovko <alik@csltd.com.ua>
2018-02-25 15:03:51 +0200

    Шаблоны

--- helloapp/app.js
+++ helloapp/app.js
@@ -3,26 +3,11 @@ var fs = require("fs");
  
 http.createServer(function(request, response){
      
-    console.log(`Запрошенный адрес: ${request.url}`);
-    if(request.url.startsWith("/public/")){
-         
-        // получаем путь после слеша
-        var filePath = request.url.substr(1);
-        fs.readFile(filePath, function(error, data){
+    fs.readFile("public/index.html", "utf8", function(error, data){
                  
-            if(error){
-                     
-                response.statusCode = 404;
-                response.end("Ресурс не найден!");
-            }   
-            else{
-                response.end(data);
-            }
-            return;
-        })
-    }
-    else{
-        // во всех остальных случаях отправляем строку hello world!
-        response.end("Hello World!");
-    }
+        var message = "Изучаем Node.js"; 
+        var header = "Главная страница";
+        data = data.replace("{header}", header).replace("{message}", message);
+        response.end(data);
+    })
 }).listen(3000);
