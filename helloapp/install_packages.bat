9bc0091fe3d64f493b35065d8fa39bd440522ec5
Alexander Golovko <alik@csltd.com.ua>
2018-02-25 15:40:29 +0200

    первое приложение с https://metanit.com/web/angular2/1.1.php

--- helloapp/install_packages.bat
+++ helloapp/install_packages.bat
@@ -1,10 +1,1 @@
-rem устанавливает пакет express и добавляет ссылку на него в package.json.dependencies
-npm install express --save
-rem устанавливает пакет jasmine-node и добавляет ссылку на него в package.json.devDependencies
-npm install jasmine-node --save-dev
-rem устанавливает пакеты, ссылки на которые указаны в package.json
-rem npm install
-rem удаляет пакет express, но оставляет ссылку на него в package.json
-rem npm uninstall express
-rem удаляет пакет express и ссылку на него в package.json.dependencies
-rem npm uninstall express --save 
+npm install
