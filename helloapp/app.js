f8f9967813aec215149c37890040a175780144c5
Alexander Golovko <alik@csltd.com.ua>
2018-02-25 14:39:44 +0200

    События

--- helloapp/app.js
+++ helloapp/app.js
@@ -1,24 +1,15 @@
-var fs = require("fs");
+var Emitter = require("events");
+var emitter = new Emitter();
+var helloEvent = "hello";
+var greetEvent = "greet";
 
-// асинхронное чтение
-fs.readFile("hello.txt", "utf8", 
-			function(error,data){
-				console.log("Асинхронное чтение файла");
-				if(error) throw error; // если возникла ошибка
-				console.log(data);	// выводим считанные данные
-				fs.writeFile("hello_out1.txt", data);
-				console.log("Синхронная запись файла завершена");
-
-
+emitter.on(greetEvent, function(){
+    console.log("Hello");
 });
 
-// синхронное чтение
-console.log("Синхронное чтение файла")
-var fileContent = fs.readFileSync("hello.txt", "utf8");
-console.log(fileContent);
-fs.writeFile("hello_out2.txt", fileContent, function(error){
-				if(error) throw error; // если возникла ошибка
-				console.log("Асинхронная запись файла завершена. Содержимое файла:");
-				var data = fs.readFileSync("hello_out2.txt", "utf8");
-				console.log(data);	// выводим считанные данные
+emitter.on(helloEvent, function(data){
+    console.log("Привет, " + data);
 });
+
+emitter.emit(greetEvent); 
+emitter.emit(helloEvent, "Мир");
